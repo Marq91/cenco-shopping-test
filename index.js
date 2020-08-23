f7f9fb1531efe38b583e9cc2592c8a8ef1c8a5d1
anibal-marquez <anibal.marquez@almacenguru.com>
2020-08-23 18:56:30 -0400

    implementation of handleProductCodes and handleTotals

--- index.js
+++ index.js
@@ -17,17 +17,39 @@ app.get("/product/:code", (req, res) => {
 
 //Need to calculate total and discount
 app.post("/checkout", (req, res) => {
-  //
-  //
-
+  // solo obtener los productos que se compraran (req.body)
+  const codes = handleProductCodes(req.body); 
+  const totals = handleTotals(req.body);
+  
   const result = {
-    total: 0,
-    totalDiscount: 0,
-    totalToPay: 0,
-    product: [],
+    total: totals.total,
+    totalDiscount: totals.totalDiscount,
+    totalToPay: totals.totalToPay,
+    product: codes,
   };
   res.json(result);
 });
+
+function handleProductCodes(products) {
+  const codesList = products.map(product => product.code);
+  return codesList
+}
+
+function handleTotals(products) {
+  let total = 0;
+  products.map(product => (total += product.price));
+
+  const discounts = products.map(product => product.discount);
+  const totalDiscount = discounts.reduce((acc, element) => acc + element, 0);
+
+  const totalToPay = total - totalDiscount;
+
+  return {
+    total: total,
+    totalDiscount: totalDiscount,
+    totalToPay: totalToPay
+  }
+}
 
 app.listen(3000);
 console.log("Express started on port 3000");
